94bbb2e1529ccb5c1ae93927873cc5e5feb8a1d4
Andrew McLagan (Rew) <andrewmclagan@gmail.com>
2024-02-29 13:27:40 +1100

    fix: update to true proxy

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM haproxy:1.9-alpine
+ARG ARCH=
+FROM ${ARCH}haproxy:1.9-alpine
 
 ENTRYPOINT ["/magic-entrypoint", "/docker-entrypoint.sh"]
 CMD ["haproxy", "-f", "/usr/local/etc/haproxy/haproxy.cfg"]
@@ -8,7 +9,7 @@ RUN apk add --no-cache python3 cmd:pip3 &&\
 
 COPY magic-entrypoint.py /magic-entrypoint
 
-ENV DEFAULT_TIMEOUT=15s
+ENV DEFAULT_TIMEOUT=30s
 
 ENV NAMESERVERS="208.67.222.222 8.8.8.8 208.67.220.220 8.8.4.4" \
     LISTEN=:100 \
@@ -22,13 +23,3 @@ ENV NAMESERVERS="208.67.222.222 8.8.8.8 208.67.220.220 8.8.4.4" \
     TIMEOUT_TUNNEL=$DEFAULT_TIMEOUT \
     UDP=0 \
     VERBOSE=0
-
-# Metadata
-ARG VCS_REF
-ARG BUILD_DATE
-LABEL org.label-schema.schema-version="1.0" \
-      org.label-schema.vendor=Tecnativa \
-      org.label-schema.license=Apache-2.0 \
-      org.label-schema.build-date="$BUILD_DATE" \
-      org.label-schema.vcs-ref="$VCS_REF" \
-      org.label-schema.vcs-url="https://github.com/Tecnativa/docker-tcp-proxy"
--- magic-entrypoint.py
+++ magic-entrypoint.py
@@ -2,25 +2,24 @@
 
 import logging
 import os
-import random
 import sys
 
 from dns.resolver import Resolver
 
 logging.root.setLevel(logging.INFO)
 
-LISTENS = os.environ["LISTEN"].split()
+LISTEN = os.environ["LISTEN"]
+TALK = os.environ["TALK"]
 NAMESERVERS = os.environ["NAMESERVERS"].split()
 resolver = Resolver()
 resolver.nameservers = NAMESERVERS
-TALKS = os.environ["TALK"].split()
 TEMPLATE = """
-backend talk_{index}
-    server stupid_{index} {talk}
+backend talk_0
+    server stupid_0 {talk}
 
-frontend listen_{index}
+frontend listen_0
     bind {listen}
-    default_backend talk_{index}
+    default_backend talk_0
 """
 config = """
 global
@@ -37,39 +36,15 @@ defaults
     timeout tunnel "$TIMEOUT_TUNNEL"
 """
 
-if len(LISTENS) != len(TALKS):
-    sys.exit("Set the same amount of servers in $LISTEN and $TALK")
-
-if os.environ["PRE_RESOLVE"] in {"0", "1"}:
-    PRE_RESOLVES = [os.environ["PRE_RESOLVE"]] * len(LISTENS)
-else:
-    PRE_RESOLVES = os.environ["PRE_RESOLVE"].split()
-
-if len(LISTENS) != len(PRE_RESOLVES):
-    sys.exit("Set the same amount of bools $PRE_RESOLVE as servers in "
-             "$LISTEN and $TALK, or use just one to set it globally")
-
-for index, (listen, talk, pre_resolve) in enumerate(zip(LISTENS, TALKS,
-                                                        PRE_RESOLVES)):
-    server, port = talk.split(":")
-    ip = server
-
-    # Resolve target if required
-    if pre_resolve == "1":
-        ip = random.choice([answer.address
-                            for answer in resolver.query(server)])
-        logging.info("Resolved %s to %s", server, ip)
-
-    # Render template
-    config += TEMPLATE.format(
-        index=index,
-        listen=listen,
-        talk=f"{ip}:{port}",
-    )
+# Render template
+config += TEMPLATE.format(
+    listen=f":{LISTEN}",
+    talk=f"host.docker.internal:{TALK}",
+)
 
 # Write template to haproxy's cfg file
 with open("/usr/local/etc/haproxy/haproxy.cfg", "w") as cfg:
     cfg.write(config)
 
-logging.info("Magic ready, executing now: %s", " ".join(sys.argv[1:]))
+logging.info(f"Proxying container:{LISTEN} TO host:{TALK}")
 os.execv(sys.argv[1], sys.argv[1:])
